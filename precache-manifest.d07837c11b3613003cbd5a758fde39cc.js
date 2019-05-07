14f68be128d405d46ab4b28f5e57e5ee0f0a21e8
Domante Stirbyte <domante.stirbyte@gmail.com>
2019-05-07 09:22:50 +0200

    Updates

---
+++ precache-manifest.d07837c11b3613003cbd5a758fde39cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6027b27c19e75c5221f42276bca3e609",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "c6833960cd20e6811af9",
+    "url": "/test-react/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c6833960cd20e6811af9",
+    "url": "/test-react/static/js/main.6864097a.chunk.js"
+  },
+  {
+    "revision": "31d3ffe18f2cb45b7bd6",
+    "url": "/test-react/static/js/runtime~main.bea1702e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
